cf3c1c580cc8b26b071f606fb5a4ecdd33d0543c
Sproff <sproff.oluwaseun@gmail.com>
2023-11-28 11:20:49 +0100

    feat: 🎸 changed envionment

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -2,11 +2,11 @@ export const getBaseUrl = () => {
 	const isNotDevelopment = process.env.NODE_ENV !== "development";
 
 	if (isNotDevelopment) {
-		return process.env.NEXT_PUBLIC_DEV_BASE_URL || "http://localhost:3000";
-	} else {
 		return (
 			process.env.NEXT_PUBLIC_PROD_BASE_URL ||
 			"https://afrilearn-api-staging.up.railway.app/v1"
 		);
+	} else {
+		return process.env.NEXT_PUBLIC_DEV_BASE_URL || "http://localhost:3000";
 	}
 };
